4360f8b432cac4576fd2c6dfe634337de45191f6
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-08-19 22:03:15 -0300

    Update dbConnection.go

--- connection/dbConnection.go
+++ connection/dbConnection.go
@@ -1,4 +1,4 @@
-package connection
+/*package connection
 
 import (
 	"fmt"
@@ -21,4 +21,31 @@ func ConnectionDB() {
 
 	fmt.Println("Base de Datos corriendo...")
 
+}*/
+
+package connection
+
+import (
+    "fmt"
+    "os"
+    "net/url"
+
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectionDB() {
+    // Lee la variable de entorno
+    dbURL := os.Getenv("MYSQL_URL")
+
+    // Abre la conexión a la base de datos
+    var err error
+    DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Base de Datos corriendo...")
 }
